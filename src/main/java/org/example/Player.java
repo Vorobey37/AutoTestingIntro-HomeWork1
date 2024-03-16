9911cd6d58c3a1f3ae8e5d091da27c7badba8652
unknown <abvorobey@gmail.com>
2024-03-16 22:06:57 +0300

    HomeWork1

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,31 @@
+package org.max.home;
+
+import org.max.seminar.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс для описания игрока
+ */
+public class Player {
+    //имя игрока
+    private String name;
+    //стратегия игрока, если true игрок меняет дверь
+    private boolean risk;
+
+    public Player(String name, boolean risk) {
+        this.name = name;
+        this.risk = risk;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getRisk() {
+        return risk;
+    }
+
+}
